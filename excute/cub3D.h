ab174ef8720f3d9ef9ac7b9cdaea69dead5bab42
dkduslee <sw56eet@gmail.com>
2026-04-19 14:22:15 +0900

    combine execute and parse

--- excute/cub3D.h
+++ excute/cub3D.h
@@ -3,23 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: solee <solee@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aylee <aylee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/27 15:51:21 by solee             #+#    #+#             */
-/*   Updated: 2026/04/07 16:53:20 by solee            ###   ########.fr       */
+/*   Created: 2026/04/12 12:20:59 by aylee             #+#    #+#             */
+/*   Updated: 2026/04/19 13:37:11 by aylee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-# define DUB3D_H
+# define CUB3D_H
 
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <math.h>
 # include <fcntl.h>
-# include "libft.h"
-# include "mlx.h"
+# include <string.h>
+# include "../Libft/libft.h"
+# include "../minilibx-linux/mlx.h"
+
+# define KEY_ESC   65307
+# define KEY_W     119
+# define KEY_S     115
+# define KEY_A     97
+# define KEY_D     100
+# define KEY_LEFT  65361
+# define KEY_RIGHT 65363
+
+# define WIDTH      640
+# define HEIGHT     480
+# define TEX_WIDTH  64
+# define TEX_HEIGHT 64
+
+# define TEX_NO 0
+# define TEX_SO 1
+# define TEX_WE 2
+# define TEX_EA 3
 
 typedef struct s_img
 {
@@ -64,10 +83,12 @@ typedef struct s_ray
 	double	side_dist_y;
 	double	delta_dist_x;
 	double	delta_dist_y;
+	double	perp_wall_dist;
 	int		step_x;
 	int		step_y;
 	int		hit;
 	int		side;
+	int		tex_num;
 }	t_ray;
 
 typedef struct s_game
@@ -80,8 +101,6 @@ typedef struct s_game
 	t_img		tex[4];
 }	t_game;
 
-//...
-
 typedef struct s_arg
 {
 	t_img	img;
@@ -91,15 +110,31 @@ typedef struct s_arg
 	t_game	game;
 }	t_arg;
 
+/* render.c */
+void	render(t_game *game);
+void	draw_ceiling(t_game *game);
+void	draw_floor(t_game *game);
+void	draw_walls(t_game *game);
 
-void	init_data(t_arg *arg, char *file);
-int	is_file_valid(char *file);
+/* raycast.c */
+void	init_ray(t_game *game, t_ray *ray, int x);
+void	calc_step_and_sidedist(t_game *game, t_ray *ray);
+void	perform_dda(t_game *game, t_ray *ray);
+void	calc_wall_height(t_game *game, t_ray *ray,
+			int *line_height, int *draw_start, int *draw_end);
+void	draw_wall_column(t_game *game, t_ray *ray,
+			int x, int line_height, int draw_start, int draw_end);
 
-int	get_window_size(t_arg *arg, t_map *map, int fd);
-int	img_parse(t_arg *arg, t_map *map, int fd);
+/* move.c */
+void	move_player(t_game *game, int keycode);
+void	rotate_player(t_game *game, int keycode);
 
-void	free_str(char **str);
+/* event.c */
+int		handle_key_press(int keycode, t_game *game);
+int		handle_window_close(t_game *game);
 
-void	exit_msg(t_arg *arg, int msg);
+/* utils (구현 필요 시) */
+int		get_pixel_color(t_img *img, int x, int y);
+void	put_pixel(t_game *game, int x, int y, int color);
 
 #endif
