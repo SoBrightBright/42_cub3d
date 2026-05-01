ca9d23d5a57e4c3fcc0c93ac3b7c170fa833ae95
Somyeong Lee <solee@c2r2s4.42gyeongsan.kr>
2026-05-01 13:17:32 +0900

    feat: crash resolve

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: solee <solee@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 11:48:30 by solee             #+#    #+#             */
+/*   Updated: 2025/04/10 14:38:29 by solee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	j;
+	int	ret;
+
+	j = 1;
+	ret = 0;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-')
+	{
+		j = -1;
+		nptr++;
+	}
+	else if (*nptr == '+')
+		nptr++;
+	while (*nptr && *nptr >= '0' && *nptr <= '9')
+	{
+		if ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+			nptr++;
+		if (*nptr >= '0' && *nptr <= '9')
+			ret = ret * 10 + *nptr - 48;
+		nptr++;
+	}
+	return (ret * j);
+}
